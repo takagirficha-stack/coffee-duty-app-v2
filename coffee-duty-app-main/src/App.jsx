9619f761a9c428eba2bf4d9ea1d1fbce137b9915
takagirficha-stack <takagi.r.ficha@gmail.com>
2026-05-26 17:02:24 +0900

    Update App.jsx

--- coffee-duty-app-main/src/App.jsx
+++ coffee-duty-app-main/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState } from "react";
 
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbzdRQAUFEGiiBVgMPHbtj61GPdxdnFLuxrj53Zydagj2NwBGMd-R3_EbDVhR18jJDkDVQ/exec";
+  "https://script.google.com/macros/s/AKfycbx41QCs8kYM81auy4klVGdwyzTQhs2t8RRpm3s2eWCiymHI-r3JjPpZ2jkOAzln_lQ_/exec";
 
 const CLEANING_MAP_URL = "/cleaning-map.jpg";
 const MACHINE_OVERVIEW_URL = "/machine-overview.png";
